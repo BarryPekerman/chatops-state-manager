9242d096ca707fb85076d7b021021f2a8fdbed9a
barry.pekerman <barrypek@gmail.com>
2025-11-15 04:53:58 +0200

    fix: add required providers to terraform-config/main.tf for Terraform 1.13.3 compatibility

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -1,3 +1,18 @@
 terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
   backend "s3" {}
+}
+
+provider "aws" {
+  # Region is set via AWS_REGION environment variable in the workflow
 }
